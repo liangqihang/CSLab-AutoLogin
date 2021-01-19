0c2ec5954313545cee0ec099b20b2693fa1dc434
Liang Qihang <liangqihang1@hotmail.com>
2021-01-19 14:56:34 +0800

    Add files via upload

---
+++ login.py
@@ -0,0 +1,32 @@
+import requests
+import argparse
+import netifaces
+
+parser = argparse.ArgumentParser(description='CSLab Login')
+parser.add_argument('--eid', metavar='N', type=str, help='CityU EID')
+parser.add_argument('--pwd', metavar='N', type=str, help='CityU password')
+args = parser.parse_args()
+
+def login():
+  headers = {
+      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Origin': 'https://cp.cs.cityu.edu.hk:16979',
+      'Connection': 'keep-alive',
+      'Referer': 'https://cp.cs.cityu.edu.hk:16979/loginform.html?',
+      'Upgrade-Insecure-Requests': '1',
+  }
+  data = {
+    'username': args.eid,
+    'ctx_pass': args.pwd,
+    'domain_name': 'CITYUMD',
+    'modify': 'Secure Login'
+  }
+  response = requests.post('https://cp.cs.cityu.edu.hk:16979/loginform.html', headers=headers, data=data)
+
+cslab = netifaces.ifaddresses('en5')[2][0]['addr'] == '10.37.1.98'
+
+if cslab:
+  login()
---
+++ login.sh
@@ -0,0 +1,1 @@
+/usr/local/anaconda3/bin/python /Users/qihang/Documents/login.py --eid qihaliang2 --pwd Qwer1234
